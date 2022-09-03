c7f5126c150a94296cdeebb9d18862f8ea137535
Hayden Johnson <haydenjohnson@MacBook-Pro.tx.rr.com>
2022-09-03 17:15:14 -0500

    p02 full credit v1

---
+++ P02/full_credit/Clock.java
@@ -0,0 +1,24 @@
+public class Clock {
+    //Constructor
+    public Clock(int hours, int minutes, int seconds){
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    //Method
+    private String twoDigit() {
+
+    }
+
+    //toString
+    @Override
+    public String toString() {
+
+    }
+
+    //Fields
+    private int hours;
+    private int minutes;
+    private int seconds;
+}
