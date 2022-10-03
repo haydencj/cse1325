d6c8122f695c1d2feefe179aa7840063586d24f4
Hayden Johnson <haydenjohnson@HaydenMBPro.tx.rr.com>
2022-10-03 13:32:33 -0500

    CF-3: Item superclass and ICF subclass

---
+++ P05/Item.java
@@ -0,0 +1,34 @@
+public class Item{
+    public Item(String name, String description, int cost, int price){
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.price = price;
+    }
+
+    public String name(){
+        return name;
+    }
+
+    public String description(){
+        return description;
+    }
+
+    public int price(){
+        return price;
+    }
+
+    public int cost(){
+        return cost;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+
+    private String name;
+    private String description;
+    private int cost;
+    private int price;
+}
