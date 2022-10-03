7a60866e33e467efd8daba528cf6539328caa7f1
Hayden Johnson <haydenjohnson@HaydenMBPro.tx.rr.com>
2022-10-03 13:42:22 -0500

    MX-3: MXA enum and MixIn class written

---
+++ P05/MixIn.java
@@ -0,0 +1,20 @@
+public class MixIn{
+    public MixIn(MixInFlavor flavor, MixInAmount amount){
+        this.flavor = flavor;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString(){
+        if(amount != MixInAmount.Normal){
+            return flavor.name() + " " + "(" + amount + ")";
+        }
+
+        else {
+            return flavor.name();
+        }
+    }
+
+    private MixInFlavor flavor;
+    private MixInAmount amount;
+}
