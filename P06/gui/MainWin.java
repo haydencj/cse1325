723dc3e112b8d1db9acd0c3221e9f3caaf76c68b
Hayden Johnson <haydenjohnson@HaydenMBPro.tx.rr.com>
2022-10-10 14:39:46 -0500

    GUI:2 Create main window and screen enum

--- P06/gui/MainWin.java
+++ P06/gui/MainWin.java
@@ -1,4 +1,59 @@
 package gui;
-public class MainWin {
+import emporium.Emporium;
+import java.awt.*;
+import javax.swing.*;
+
+public class MainWin extends JFrame {
+    public MainWin(String title){
+        super(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,600);
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu file = new JMenu("File");
+        JMenuItem quit = new JMenuItem("Quit");
+
+        JMenu view = new JMenu("View");
+        JMenuItem icFlavorView = new JMenuItem("Ice Cream Flavor");
+        JMenuItem mxFlavorView = new JMenuItem("Mix In Flavor");
+        JMenuItem sFlavorView = new JMenuItem("Scoop");
+
+        JMenu create = new JMenu("Create");
+        JMenuItem icFlavorCreate = new JMenuItem("Ice Cream Flavor");
+        JMenuItem mxFlavorCreate= new JMenuItem("Mix In Flavor");
+        JMenuItem sFlavorCreate = new JMenuItem("Scoop");
+
+        JMenu help = new JMenu("Help");
+        JMenuItem about = new JMenuItem("About");
+
+        quit.addActionListener(event -> onQuitClick());
+    }
+
+    public void onQuitClick(){
+
+    }
+
+    public void onCreateIceCreamFlavorClick(){
+
+    }
+
+    public void onCreateMixInFlavorClick(){
+
+    }
+
+    public void onCreateScoopClick(){
+
+    }
+
+    public void onAboutClick(){
+
+    }
+
+    private void view(Screen screen){
+
+    }
     
+    private Emporium emporium;
+    private JLabel display;
 }
