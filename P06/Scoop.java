793d99b28c15ab95433be8e53b561e67752edd61
Hayden Johnson <haydenjohnson@HaydenMBPro.tx.rr.com>
2022-10-10 13:20:55 -0500

    initial file transfer from p05

---
+++ P06/Scoop.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Scoop{
+    public Scoop(IceCreamFlavor flavor){
+        this.flavor = flavor;
+        this.mixins = new ArrayList<>();
+    }
+
+    public void addMixIn(MixIn mixin){
+        mixins.add(mixin);
+    }
+
+    @Override
+    public String toString(){
+        if(mixins.isEmpty() == true){
+            return flavor.name();
+        }
+
+        else if(mixins.size()==1){
+            return flavor.name() + "with" + mixins;
+        }
+
+        else{ //i.e. more than one mix in
+            String result = mixins.get(0).toString();
+            for(int i=1; i<mixins.size(); i++){
+                result += ", " + mixins.get(i);
+            }
+
+            return flavor.name() + " with " + result;
+        }
+    }
+
+    private IceCreamFlavor flavor;
+    private ArrayList<MixIn> mixins;
+}
