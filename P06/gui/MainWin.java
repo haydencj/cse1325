5314db5ecd79df27afa905712d17c64c6de03f01
Hayden Johnson <haydenjohnson@HaydenMBPro.tx.rr.com>
2022-10-10 16:59:43 -0500

    GUI:3 Add menu bar

--- P06/gui/MainWin.java
+++ P06/gui/MainWin.java
@@ -28,10 +28,41 @@ public class MainWin extends JFrame {
         JMenuItem about = new JMenuItem("About");
 
         quit.addActionListener(event -> onQuitClick());
+
+        icFlavorView.addActionListener(event -> view(Screen.ICE_CREAM_FLAVORS));
+        mxFlavorView.addActionListener(event -> view(Screen.MIX_IN_FLAVORS));
+        sFlavorView.addActionListener(event -> view(Screen.SCOOPS));
+
+        icFlavorCreate.addActionListener(event -> onCreateIceCreamFlavorClick());
+        mxFlavorCreate.addActionListener(event -> onCreateMixInFlavorClick());
+        sFlavorCreate.addActionListener(event -> onCreateScoopClick());
+
+        about.addActionListener(event -> onAboutClick());
+
+        file.add(quit);
+
+        view.add(icFlavorView);
+        view.add(mxFlavorView);
+        view.add(sFlavorView);
+
+        create.add(icFlavorCreate);
+        create.add(mxFlavorCreate);
+        create.add(sFlavorCreate);
+
+        help.add(about);
+
+        menuBar.add(file);
+        menuBar.add(view);
+        menuBar.add(create);
+        menuBar.add(help);
+        setJMenuBar(menuBar);
+
+        display = new JLabel();
+        setVisible(true);
     }
 
     public void onQuitClick(){
-
+        System.exit(0);
     }
 
     public void onCreateIceCreamFlavorClick(){
@@ -47,13 +78,13 @@ public class MainWin extends JFrame {
     }
 
     public void onAboutClick(){
-
+        JDialog about = new JDialog();
     }
 
     private void view(Screen screen){
 
     }
-    
+
     private Emporium emporium;
     private JLabel display;
 }
