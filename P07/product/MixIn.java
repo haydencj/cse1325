fa27908328ac1f6fe9edb385953dc0ffb7d0cc51
Hayden Johnson <haydenjohnson@HaydenMBPro.tx.rr.com>
2022-10-16 15:19:38 -0500

    P07 initial file transfer

---
+++ P07/product/MixIn.java
@@ -0,0 +1,21 @@
+package product;
+public class MixIn{
+    public MixIn(MixInFlavor flavor, MixInAmount amount){
+        this.flavor = flavor;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString(){
+        if(amount != MixInAmount.Normal){
+            return flavor.name() + " " + "(" + amount + ")";
+        }
+
+        else {
+            return flavor.name();
+        }
+    }
+
+    private MixInFlavor flavor;
+    private MixInAmount amount;
+}
---
+++ P07/product/MixInFlavor.java
@@ -0,0 +1,6 @@
+package product;
+public class MixInFlavor extends Item{
+    public MixInFlavor(String name, String description, int cost, int price){
+        super(name, description, cost, price);
+    }
+}
