bcf08d1f62dcb19f52dae78b4c6e07c3e36df617
Hayden Johnson <haydenjohns02@gmail.com>
2022-11-07 16:47:29 -0600

    p09 cont

--- P09/full_credit/Matrix3.cpp
+++ P09/full_credit/Matrix3.cpp
@@ -4,6 +4,8 @@
 Matrix3::Matrix3(int m00, int m10, int m20, int m01, int m11, int m21, int m02, int m12, int m22) 
     : data{{m00,m10,m20},{m01,m11,m21},{m02,m12,m22}} { }
 
+Matrix3::Matrix3() : data{{0,0,0},{0,0,0},{0,0,0}} { }
+
 int Matrix3::get(int x, int y) {
     if(x > 2 || x < 0) throw std::runtime_error("Y index out of range");
     if(y > 2 || y < 0) throw std::runtime_error("Y index out of range");
@@ -32,9 +34,9 @@ std::ostream& operator<<(std::ostream& out, Matrix3& m) {
     {
         for(j = 0; j < 3; ++j)
         {
-            out << m.data[i][j];
+            out << m.data[i][j] << " ";
         }
-        out << "\n";
+        out << std::endl;
     }
     return out;
 }
--- P09/full_credit/Matrix3.h
+++ P09/full_credit/Matrix3.h
@@ -5,10 +5,11 @@
 class Matrix3 {
     public:
         Matrix3(int m00, int m10, int m20, int m01, int m11, int m21, int m02, int m12, int m22);
+        Matrix3();
         int get(int x, int y);
         Matrix3 operator+(Matrix3 rhs);
-        friend std::ostream& operator<<(std::ostream out, Matrix3 m);
-        friend std::istream& operator>>(std::istream in, Matrix3 m);
+        friend std::ostream& operator<<(std::ostream& out, Matrix3& m);
+        friend std::istream& operator>>(std::istream& in, Matrix3& m);
     private:
         std::vector<std::vector<int>> data;
 };
